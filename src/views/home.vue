5633386da4436a7afc2473f8ad140845212e6689
ViniciusAcordiSoethe <vinicius.soethe@proton.me>
2023-10-26 22:41:47 -0300

    feat: colocando .env para produção do versel

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TopMenu from '@/components/topMenu.vue';
 import Footer from '@/components/footer.vue';
+
+const ssss = import.meta.env.VITE_TESTENV;
 </script>
 <template>
     <TopMenu />
     <h1>Home</h1>
-    <p>Aqui ficara o estudo de Vue Completo</p>
+    <p>Aqui ficara o estudo de Vue Completo {{ ssss }}</p>
     <Footer />
 </template>
 <style scoped>
