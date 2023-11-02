819aa308000defc23941769bdb630359816486c6
ViniciusAcordiSoethe <vinicius.soethe@proton.me>
2023-11-02 10:46:41 -0300

    fix : Corrigindo a nomeclatura do testeEnv

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,13 +2,12 @@
 import TopMenu from '@/components/topMenu.vue';
 import Footer from '@/components/footer.vue';
 
-const ssss = import.meta.env.VITE_TESTENV;
+const testeEnv = import.meta.env.VITE_TESTENV;
 </script>
 <template>
     <TopMenu />
     <h1>Home</h1>
-    <p>Aqui ficara o estudo de Vue Completo {{ ssss }}</p>
-    <p>Oi arthur</p>
+    <p>Aqui ficara o estudo de Vue Completo {{ testeEnv }}</p>
     <Footer />
 </template>
 <style scoped>
